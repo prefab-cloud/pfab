605b4d836a86c694a3a0c645404181be4b636673
Jeff Dwyer <jdwyah@gmail.com>
2018-12-29 00:31:38 -0500

    switch to actual k8 job for jobs

--- lib/pfab/templates/job.rb
+++ lib/pfab/templates/job.rb
@@ -7,36 +7,52 @@ module Pfab
 
       def job
         {
-          kind: "Pod",
-          apiVersion: "v1",
+          apiVersion: "batch/v1",
+          kind: "Job",
           metadata: {
-            name: "#{@data['deployed_name']}-#{@data['sha']}",
+            name: "job-#{@data['deployed_name']}-#{@data['sha']}",
             namespace: @data['env'],
             labels: {
               application: @data['application'],
               "deployed-name" => @data['deployed_name'],
+              "application-type" => "job",
             }
           },
           spec: {
-            containers: [
-              {
-                image: image_name,
-                name: @data['deployed_name'],
-                command: app_vars["command"].split(" "),
-                env: env_vars,
-                resources: {
-                  requests: {
-                    cpu: @data["cpu"] || "50m",
-                    memory: @data["memory"] || "256Mi",
-                  },
-                  limits: {
-                    cpu: @data["cpu"] || "250m",
-                    memory: @data["memory"] || "256Mi",
-                  },
+            template: {
+              metadata: {
+                name: "#{@data['deployed_name']}-#{@data['sha']}",
+                namespace: @data['env'],
+                labels: {
+                  application: @data['application'],
+                  "deployed-name" => @data['deployed_name'],
+                  "application-type" => "job",
                 },
-              }
-            ]
-          }
+              },
+              spec: {
+                containers: [
+                  {
+                    image: image_name,
+                    name: @data['deployed_name'],
+                    command: app_vars["command"].split(" "),
+                    env: env_vars,
+                    resources: {
+                      requests: {
+                        cpu: @data["cpu"] || "50m",
+                        memory: @data["memory"] || "256Mi",
+                      },
+                      limits: {
+                        cpu: @data["cpu"] || "250m",
+                        memory: @data["memory"] || "256Mi",
+                      },
+                    },
+                  },
+                ],
+                restartPolicy: "Never",
+              },
+            },
+            backoffLimit: 0,
+          },
         }
       end
     end
