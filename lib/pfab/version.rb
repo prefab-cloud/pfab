da1c497bab6a6600a88d1f387433f154b509fb3c
James Kebinger <james.kebinger@prefab.cloud>
2024-05-22 14:43:13 -0500

    version bump

--- lib/pfab/version.rb
+++ lib/pfab/version.rb
@@ -1,8 +1,8 @@
 module Pfab
   module Version
     MAJOR = 0
-    MINOR = 54
-    PATCH = 1
+    MINOR = 55
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
